b2fec3abe94d6f3f16772075533ac0e6ea3d3830
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-11 14:41:37 +0530

    Update CrawlImages.js

--- src/CrawlImages.js
+++ src/CrawlImages.js
@@ -30,7 +30,7 @@ function CrawlImages() {
 
     const fetchImageData = async () => {
         try {
-            const response = await fetch("https://localhost:5000/img-members");
+            const response = await fetch("http://localhost:5000/img-members");
             if (!response.ok) {
                 throw new Error('Failed to fetch image data');
             }
@@ -43,7 +43,7 @@ function CrawlImages() {
     };
 
     const handleImageDownload = () => {
-        fetch('https://localhost:5000/img-download')
+        fetch('http://localhost:5000/img-download')
             .then(response => response.blob())
             .then(blob => {
                 const url = window.URL.createObjectURL(new Blob([blob]));
